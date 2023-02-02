3edd9ee69a19aa62b22c1749db34416c7b959949
Teja D <62719821+tejaDhulipala@users.noreply.github.com>
2023-02-02 12:50:43 -0500

    Add files via upload

--- PlanetClass.py
+++ PlanetClass.py
@@ -1,12 +1,12 @@
 import pygame as pg
 import random
-from math import log
+from math import log, pow
 
 # The conversion of the mass to the radius, how much the mass is multiplied by to get the radius
 from pygame.math import Vector2
 
-massToRadius = 5
-G = 0.005
+#massToRadius = 1
+G = 1
 
 
 class Planet:
@@ -14,25 +14,35 @@ class Planet:
     pos: Vector2
 
     def __init__(self, pos, v, m, system: list, color=(random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256)), a=pg.Vector2(0, 0)):
+        self.initpos = pg.math.Vector2(pos)
+        self.initv = pg.math.Vector2(v)
         self.pos = pg.math.Vector2(pos)
         self.v = pg.math.Vector2(v)
         self.m = m
         self.color = color
         self.system = system
         self.a = a
+        self.isRunning = False
         system.append(self)
 
     def draw(self, screen):
-        pg.draw.circle(screen, self.color, self.pos, log(self.m + 1) * massToRadius)
+        pg.draw.circle(screen, self.color, self.pos, 12)
+        #pg.draw.line(screen, (255, 0, 0), self.pos, self.pos + self.a * 10000, 2)
+
+    def changeVals(self, t):
+        if self.isRunning:
+            f = pg.Vector2(0, 0)
+            # The sum of the forces acting on the planets
+            # |F| = Gm1m2 ./ r ^2
+            for planet in self.system:
+                if planet != self:
+                    mag = G * self.m * planet.m / (self.pos - planet.pos).length_squared()
+                    if mag > (planet.pos - self.pos).length():
+                        mag = (planet.pos - self.pos).length()
+                    f += mag * (planet.pos - self.pos) / (planet.pos - self.pos).length()
+            self.a = f / self.m
+            self.v += self.a * t
 
     def changePos(self, t):
-        f = pg.Vector2(0, 0)
-        # The sum of the forces acting on the planets
-        # F = G * m1 * m2  r^2
-        for planet in self.system:
-            if planet != self:
-                f += (G * self.m * planet.m / (self.pos - planet.pos).length_squared()) * (planet.pos - self.pos)
-        # Now that we have the sum of the forces as a vector, we have to convert that into acceleration
-        self.a += f / self.m * t
-        self.v += self.a
-        self.pos += self.v
+        if self.isRunning:
+            self.pos += self.v * t
