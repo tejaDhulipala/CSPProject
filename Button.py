0e6cc5073658addfac25ef7d9613b47f5244b893
Teja D <62719821+tejaDhulipala@users.noreply.github.com>
2023-02-02 12:50:19 -0500

    Add files via upload

---
+++ Button.py
@@ -0,0 +1,37 @@
+import pygame as pg
+
+
+class GameButton:
+    def __init__(self, pos, text, color, width, height, callback, textHeight=20):
+        self.pos = pos
+        self.text = text
+        self.callback = callback
+        self.color = color
+        self.width = width
+        self.height = height
+        self.textHeight = textHeight
+
+    def draw(self, screen):
+        pg.draw.rect(screen, (255, 255, 255), pg.Rect(self.pos, (self.width, self.height)), border_radius=5)
+        font = pg.font.SysFont("Arial", self.textHeight)
+        text = font.render(self.text, True, self.color)
+        screen.blit(text, self.pos)
+
+    def checkPos(self, pos):
+        if self.pos[0] <= pos[0] <= self.pos[0] + self.width and self.pos[1] <= pos[1] <= self.pos[1] + self.height:
+            self.callback()
+
+
+# Control Functions
+def run(sys):
+    for planet in sys:
+        planet.isRunning = True
+
+
+def reset(sys):
+    for planet in sys:
+        planet.v = planet.initv
+        planet.pos = planet.initpos
+        planet.isRunning = False
+        print(planet.pos)
+        print(planet.initpos)
